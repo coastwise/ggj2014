2f616e40e8915823c98796df3b7bd461f4358757
michellejs <skolly@Michelles-MacBook-Pro.local>
2014-01-25 01:17:11 -0500

    Game Flow State Machine Finished Tile Templates Finished
    Game Flow State Machine Working - States Menu, Play and EndGame.

---
+++ Assets/Scripts/EndGameState.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public sealed class EndGameState :  State<GameManager> {
+
+	
+	static readonly EndGameState instance = new EndGameState();
+	public static EndGameState Instance {
+		get 
+		{
+			return instance;
+		}
+	}
+	static EndGameState() { }
+	private EndGameState() { }
+	
+	public override void Enter (GameManager g) {
+		if (g.Location != Locations.endGameState) {
+			g.ChangeLocation(Locations.endGameState);
+			//EndGame.Instance.playDisplayed = true;
+		}
+	}
+	public override void Execute (GameManager g) {
+		//if (EndGame.Instance.finishPressed)				
+			SetNextState (g, "mainMenu");
+		//EndGame.Instance.finishPressed = false;
+	}
+	
+	public override void Exit(GameManager g) {
+		Play.Instance.playDisplayed = false;
+		Debug.Log("Exiting Endgame");
+	}
+
+	public override void SetNextState(GameManager g, string state)//This needs to be removed in all states once I get the GUI working
+	{
+		g.ChangeState (state);
+	}
+}
+
+
