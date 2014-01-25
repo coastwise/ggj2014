be6e87079f2e35b63e1a68c88a23d328267b6f59
Farzad Afshari <farzad.afshari@wickedroar.com>
2014-01-25 16:33:42 -0500

    Created Player controls to throw boomerang
    Boomerang has two states, Go and Return

--- Assets/Scripts/Boomerang.cs
+++ Assets/Scripts/Boomerang.cs
@@ -4,16 +4,33 @@ using System.Collections;
 public class Boomerang : MonoBehaviour {
 
 	public GameObject _origin;
+	public Vector3 _spawnPoint;
+	public Vector3 _throwDirection;
+	public float _speed;
+	public float _spawnTime;
 	Vector3 _velocity;
+
+	public delegate void BoomerangUpdate();
+
+	public BoomerangUpdate boomerangUpdate;
 	// Use this for initialization
 	void Start () {
-		_velocity = new Vector3(14,0,0);
+	}
+
+	public void Init(float speed, Vector3 spawnPoint, Vector3 throwDirection) {
+		transform.position = _spawnPoint;
+		_speed = speed;
+		_spawnPoint = spawnPoint;
+		_throwDirection = throwDirection;
+		_spawnTime = Time.time;
+		transform.localRotation = Quaternion.identity;
+		boomerangUpdate = SineGo;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Rotate (540);
-		BasicReturn ();
+		Rotate (720);
+		boomerangUpdate ();
 	}
 
 	/// <summary>
@@ -24,20 +41,21 @@ public class Boomerang : MonoBehaviour {
 		transform.Rotate(Vector3.forward, -angle * Time.deltaTime);
 	}
 
-	/// <summary>
-	/// Basics the return.
-	/// </summary>
-	void BasicReturn () {
+	void SineReturn () {
 		Vector3 delta = _origin.transform.position - transform.position;
-
-		Vector3 moreDelta = delta * 1.1f + _velocity;
-
-		if (moreDelta.magnitude > 14) {
-		moreDelta.Normalize();
-		moreDelta *= 14;
-		}
-		_velocity = moreDelta;
-
-		transform.Translate(_velocity * Time.deltaTime, Space.World);
+		transform.Translate (delta.normalized * _speed * Time.deltaTime, Space.World);
 	}
+
+
+	void SineGo () {
+
+		transform.Translate(_throwDirection * _speed * Mathf.Sin((Time.time - _spawnTime) * 4f + Mathf.Deg2Rad * 90) * Time.deltaTime, Space.World);
+
+
+		if (Mathf.Sin (Time.time - _spawnTime) < Mathf.Sin (Time.time - _spawnTime - Time.deltaTime)) {
+			// Change boomerangs collision flags to include player
+			boomerangUpdate = SineReturn;
+		}
+	}
+
 }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public Boomerang _boomerang;
+	bool hasBoomerang = true;
+	public GameObject spawnPointBoomerang;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space) && hasBoomerang) {
+			InitBoomerang();
+			EnableBoomerang();
+		}
+	}
+
+	void InitBoomerang() {
+		_boomerang.Init(transform.localScale.x * 20,
+		                transform.position + (Vector3.right * transform.localScale.x * 1.25f),
+		                Vector3.right * transform.localScale.x);
+	}
+
+	void EnableBoomerang () {
+		_boomerang.gameObject.SetActive(true);
+		hasBoomerang = false;
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		if (collider.gameObject.Equals(_boomerang.gameObject)) {
+			_boomerang.gameObject.SetActive(false);
+			hasBoomerang = true;
+		}
+	}
+}
