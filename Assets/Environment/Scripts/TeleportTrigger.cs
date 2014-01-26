2f6b3633a3919d9744a48d5f68f1ad6a9151ae14
Patrick McKenna <patrick.a.mckenna@gmail.com>
2014-01-26 09:25:09 -0500

    add horizontal level wraping

---
+++ Assets/Environment/Scripts/TeleportTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeleportTrigger : MonoBehaviour {
+
+	public Transform destination;
+
+	private Vector3 delta;
+
+	void Start () {
+		delta = destination.position - transform.position;
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		other.gameObject.transform.position += delta;
+	}
+
+}
