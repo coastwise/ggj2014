25f88e97dcde76209ec1e796d2805c4402d35cbb
Patrick McKenna <patrick.a.mckenna@gmail.com>
2014-01-26 15:11:03 -0500

    force 1024x576 resolution

--- Assets/HUD/GuiGameObject.cs
+++ Assets/HUD/GuiGameObject.cs
@@ -13,6 +13,8 @@ public class GuiGameObject : MonoBehaviour
 
 	void Start()
 	{
+		Screen.SetResolution(512,288,true);
+		Screen.SetResolution(1024,576,true);
 		_pointerTextures = new Texture[] {_textureP1Pointer, _textureP2Pointer, _textureP3Pointer, _textureP4Pointer};
 
 		PlayerController[] players = (PlayerController[])GameObject.FindObjectsOfType(typeof(PlayerController));
@@ -30,10 +32,10 @@ public class GuiGameObject : MonoBehaviour
 		for (int i = 0; i < _playerTransforms.Length; i += 1)
 		{
 			Vector3 playerPosition = Camera.main.WorldToScreenPoint(_playerTransforms[i].position);
-			GUI.DrawTexture(new Rect(playerPosition.x - _pointerHalfWidths[i], 576.0f - playerPosition.y - 66.0f, _pointerFullWidths[i], 30.0f), _pointerTextures[i]);
+			GUI.DrawTexture(new Rect(playerPosition.x - _pointerHalfWidths[i], (float)Screen.height - playerPosition.y - 66.0f, _pointerFullWidths[i], 30.0f), _pointerTextures[i]);
 
 			float xOffset = playerPosition.x - 10.0f;
-			float yOffset = 576.0f - playerPosition.y - 78.0f;
+			float yOffset = (float)Screen.height - playerPosition.y - 78.0f;
 			int tempFireableBoomerangs = _playerControllers[i]._fireableBoomerangs;
 			//int tempFireableBoomerangs = 9;
 			int tempCounter = 0;
