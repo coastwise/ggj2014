f24ea2a2c66666e12aa2ffc97774c9eeaf6b8ad2
Farzad Afshari <farzad.afshari@wickedroar.com>
2014-01-26 03:25:13 -0500

    Created Start Screen with Wicked Roar Logo

---
+++ Assets/Start Screen/StartScreen.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreen : MonoBehaviour {
+
+	public SpriteRenderer[] splashImages;
+
+	int curSplash = 0;
+	bool isVisible = false;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+		float color = Mathf.Sin (Time.timeSinceLevelLoad - Mathf.Deg2Rad * 89);
+
+
+		splashImages[curSplash].color = new Color(color, 
+		                                          color, 
+		                                          color);
+
+		isVisible = color > 0;
+
+		//Debug.Log (color);
+		//Debug.Log (Mathf.Sin (Time.timeSinceLevelLoad - Mathf.Deg2Rad * 89 - Time.deltaTime));
+
+		if (!isVisible && color < Mathf.Sin (Time.timeSinceLevelLoad - Mathf.Deg2Rad * 89 - Time.deltaTime)) {
+			if (++curSplash > splashImages.Length)
+				Application.Quit();
+				// load the next scene
+		}
+
+	}
+}
