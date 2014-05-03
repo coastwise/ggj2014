d265d5136d6860fb4749292a9dd3da49880cf31f
Kyle Van Grinsven <kyle.vangrinsven@wickedroar.com>
2014-05-03 02:40:43 -0400

    Character Select Screen

---
+++ Assets/Character Select/CharacterSelect.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSelect : MonoBehaviour {
+
+	public static bool yellowPlayer;
+	public static bool greenPlayer;
+	public static bool redPlayer;
+	public static bool bluePlayer;
+
+	private GameObject p1;
+	private GameObject p2;
+	private GameObject p3;
+	private GameObject p4;
+
+	private GUIStyle subtitle_style;
+
+	// Use this for initialization
+	void Start () {
+		p1 = GameObject.Find("p1");
+		p2 = GameObject.Find("p2");
+		p3 = GameObject.Find("p3");
+		p4 = GameObject.Find("p4");
+
+		subtitle_style = new GUIStyle();
+		subtitle_style.normal.textColor = Color.white;
+		subtitle_style.alignment = TextAnchor.MiddleCenter;
+		subtitle_style.fontStyle = FontStyle.Normal;
+		subtitle_style.fontSize = 40;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Start_1") && !yellowPlayer){
+			yellowPlayer = true;
+			p1.guiTexture.enabled = true;
+		}
+		else if(Input.GetButtonDown("Start_2") && !greenPlayer){
+			greenPlayer = true;
+			p2.guiTexture.enabled = true;
+		}
+		else if(Input.GetButtonDown("Start_3") && !redPlayer){
+			redPlayer = true;
+			p3.guiTexture.enabled = true;
+		}
+		else if(Input.GetButtonDown("Start_4") && !bluePlayer){
+			bluePlayer = true;
+			p4.guiTexture.enabled = true;
+		}
+		else{
+			// Proceed
+			if(Input.GetButtonDown("Start_1") && yellowPlayer && (greenPlayer || redPlayer || bluePlayer)){
+				// Go to settings
+				Debug.Log("success");
+			}
+			else if(Input.GetButtonDown("Start_2") && greenPlayer && (yellowPlayer || redPlayer || bluePlayer)){
+				// Go to settings
+				Debug.Log("success");
+			}
+			else if(Input.GetButtonDown("Start_3") && redPlayer && (yellowPlayer || greenPlayer || bluePlayer)){
+				// Go to settings
+				Debug.Log("success");
+			}
+			else if(Input.GetButtonDown("Start_2") && bluePlayer && (yellowPlayer || greenPlayer || redPlayer)){
+				// Go to settings
+				Debug.Log("success");
+			}
+		}
+	}
+
+	void OnGUI(){
+		float x = 0.4f * Screen.width;
+		float y = 0.01f * Screen.height;
+		float w = 0.2f * Screen.width;
+		float h = 0.1f * Screen.height;
+		float padding = 0.02f * Screen.height;
+		
+		GUI.TextArea(new Rect(x,y,w,h), "Press start to join", subtitle_style);
+	}
+}
